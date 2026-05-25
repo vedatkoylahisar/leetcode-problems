aded6040624aab7f327265dbd029c0c469fe477d
vedatkoylahisar <112508838+vedatkoylahisar@users.noreply.github.com>
2026-05-25 21:51:57 +0300

    Time: 0 ms (100%), Space: 46.5 MB (87.26%) - LeetHub

--- 0027-remove-element/0027-remove-element.cs
+++ 0027-remove-element/0027-remove-element.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int index = 0; 
-
-        for (int i = 0; i < nums.Length; i++) {
-            if (nums[i] != val) {
-                nums[index] = nums[i]; 
-                index++;
+        int k=0;
+        for(int i = 0; i<nums.Length; i++)
+        {
+            if(nums[i]!=val)
+            {
+                 nums[k]=nums[i];
+                 k++;
             }
         }
-
-        return index; // Yeni dizinin uzunluğu
+        return k;
     }
 }
